80ce83276027b233f02d8a463f66332d43f62622
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-07 23:13:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
-function destruivelyPrependKitten(name) {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens
 }
